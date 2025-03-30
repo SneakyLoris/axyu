d97957278ae9a5b099c0f74fc224725212e59ef1
Admin <tankn.qaz@gmail.com>
2025-03-30 23:21:11 +0300

    added registration\login\logout

--- web/urls.py
+++ web/urls.py
@@ -6,8 +6,8 @@ from web.views import *
 urlpatterns = [
     path('', main_view, name='main'),
     path('registration', registration_view, name='registration'),
-    path('auth', auth_view, name='auth'),
+    path('login', auth_view, name='login'),
+    path('logout', logout_view, name='logout'),
     path('goods/<str:category_name>', goods_view, name='goods'),
     path('goods/<int:item_id>', good_view, name='good'),
-
 ]
