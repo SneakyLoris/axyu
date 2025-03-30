854521faa61f9ba79241a85aa4338a23a12f6d48
Admin <tankn.qaz@gmail.com>
2025-03-30 18:39:06 +0300

    created navigation links

--- web/views.py
+++ web/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def main_view(request):
     return render(request, "web/main.html")
+
+
+def registration_view(request):
+    pass
+
+
+def auth_view(request):
+    pass
+
+
+def goods_view(request, value):
+    pass
+
+
+def good_view(request, value):
+    pass
