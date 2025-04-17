a32f13e9770856662831624cec3d423e54b8728f
Admin <tankn.qaz@gmail.com>
2025-04-18 01:59:11 +0300

    refactored views | added css

--- web/urls.py
+++ web/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('logout', logout_view, name='logout'),
     path('goods/<str:category_name>', goods_view, name='goods'),
     path('goods/<int:item_id>', good_view, name='good'),
+    path('stats/', good_view, name='stats'),  # ТЕСТОВЫЙ РЕЖИМ!!!!
+    path('tests/', good_view, name='tests'),  # ТЕСТОВЫЙ РЕЖИМ!!!!
+    path('categories/add', good_view, name='add_category'),  # ТЕСТОВЫЙ РЕЖИМ!!!!
 ]
