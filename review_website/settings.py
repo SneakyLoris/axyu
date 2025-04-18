931d6c38b798c3e2daf0f6aac9ee1b286107d29e
Admin <tankn.qaz@gmail.com>
2025-04-18 21:03:21 +0300

    ьÑmerged html and css files in web app

--- review_website/settings.py
+++ review_website/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static", "web"),
+   os.path.join(BASE_DIR, "web", "static"),
 ]
 
 # Default primary key field type
