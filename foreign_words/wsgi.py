3984b4792640de8ad8e2a7b0648b33aa7dc9aec3
NikitaNavrotskiy <NikitaNavrotskiy@mail.ru>
2025-04-18 23:14:17 +0300

    fixed: name of project: review_website -> foreign_words

--- foreign_words/wsgi.py
+++ foreign_words/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for review_website project.
+WSGI config for foreign_words project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'review_website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foreign_words.settings')
 
 application = get_wsgi_application()
