f3dcde06fa691c66fc98b8034030f28d0a2e7d46
Rolan <tankn.qaz@gmail.com>
2025-04-29 14:16:26 +0300

    copied code from Mika files

--- web/views.py
+++ web/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timedelta
+from random import random
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.db.models import Q, Case, When, Value, Exists, OuterRef, CharField
@@ -161,3 +164,37 @@ def feedback_view(request):
         form = FeedbackForm()
     
     return render(request, 'web/feedback.html', {'form': form})
+
+
+def ultra_simple_dashboard(request):
+    user = request.user
+
+    stats = {
+        'total_words': random.randint(50, 200),
+        'total_quizzes': random.randint(10, 50),
+        'success_rate': random.randint(60, 95),
+    }
+
+    # категории
+    categories = {
+        'Глаголы': random.randint(10, 40),
+        'Существительные': random.randint(10, 40),
+        'Прилагательные': random.randint(5, 20),
+        'Фразы': random.randint(5, 15),
+    }
+
+    # рузультат за неделю
+    week_dates = [(datetime.now() - timedelta(days=i)).strftime('%d.%m') for i in range(7)]
+    week_progress = [
+        {'date': date, 'words': random.randint(1, 10), 'quizzes': random.randint(0, 3)}
+        for date in week_dates
+    ]
+
+    context = {
+        'stats': stats,
+        'categories': categories,
+        'week_progress': week_progress,
+    }
+
+    # вот тут гпт
+    return render(request, 'web/stats.html', context)
