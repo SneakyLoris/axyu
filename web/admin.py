9e6015d1fe9111dc62402e1df05ca4c464485f34
Rolan <tankn.qaz@gmail.com>
2025-05-06 15:31:05 +0300

    registred models at admin.py

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from api.models import Category
+from api.models import *
 
 # Register your models here.
 admin.site.register(Category)
+admin.site.register(Word)
+admin.site.register(Learning_Session)
+admin.site.register(Answer_Attempt)
+admin.site.register(Word_Repetition)
+admin.site.register(Learning_Category)
+admin.site.register(Learned_Word)
+admin.site.register(Feedback)
