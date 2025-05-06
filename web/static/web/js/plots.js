08232d40f7210e1dad35d2e7e71b9517ae1baf73
Rolan <tankn.qaz@gmail.com>
2025-05-06 20:06:34 +0300

    pie plot fixed

--- web/static/web/js/plots.js
+++ web/static/web/js/plots.js
@@ -1,25 +1,11 @@
-var test_data = [{
-  values: [19, 26, 55],
-  labels: ['Residential', 'Non-Residential', 'Utility'],
-  type: 'pie'
-}];
-
-var raw_data = JSON.parse(document.getElementById('pie-data')
-
-var pie_data = [{
-    values: raw_data.values,
-    labels: raw_data.labels,
-    type: 'pie'
-}];
+const pie_data = JSON.parse(document.getElementById('pie-data').textContent);
 
 var layout = {
   height: 400,
   width: 500
 };
 
-Plotly.newPlot('piePlot', [{
-  values: [19, 26, 55],
-  labels: ['Residential', 'Non-Residential', 'Utility'],
-  type: 'pie'
-}];, layout);
+Plotly.newPlot('piePlot', pie_data, layout);
+
+
 
